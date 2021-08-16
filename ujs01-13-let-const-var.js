959c9f761cd7a74a6c2f8046fb3a2981cbd5abc9
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-16 18:32:32 +0300

    Let, const, var

---
+++ ujs01-13-let-const-var.js
@@ -0,0 +1,17 @@
+////////////////////////////////////
+// let, const and var
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+// birthYear = 1990;    // cannot reasigne value to const variable
+// const job;
+console.log(birthYear);
+
+var job = 'programmer';
+job = 'teacher'
+console.log(job);
+
+lastName = 'Schmedtmann';
+console.log(lastName);
